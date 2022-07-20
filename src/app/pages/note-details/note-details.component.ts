ca41cac40ec73ecca3f17559279a8d0dc29638c7
santiago julian monsalve <s.julian.monsalve@accenture.com>
2022-07-20 13:34:27 +0200

    updating functionality working

--- src/app/pages/note-details/note-details.component.ts
+++ src/app/pages/note-details/note-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Note } from 'src/app/shared/note.model';
 import { NotesService } from 'src/app/shared/notes.service';
 
@@ -11,16 +11,35 @@ import { NotesService } from 'src/app/shared/notes.service';
 })
 export class NoteDetailsComponent implements OnInit {
   note!: Note;
+  noteId!: number;
+  new!: boolean;
 
-  constructor(private notesService : NotesService, private router: Router) { }
+  constructor(private notesService : NotesService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.note = new Note()
+    this.route.params.subscribe((params: Params) => {
+      this.note = new Note()
+
+      if(params['id']) {
+        this.note = this.notesService.get(params['id']);
+        this.noteId = params['id'];
+        this.new = false;
+      } else {
+        this.new = true;
+      }
+    })
+
   }
 
   onSubmit(form:NgForm) {
-    this.notesService.add(form.value)
+    if(this.new) {
+      this.notesService.add(form.value)
+    } else {
+      this.notesService.update(this.noteId, form.value.title, form.value.body)
+    }
     this.router.navigateByUrl('/');
+
+
   }
 
   cancel() {
